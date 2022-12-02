951e73331fa81230570e999f14a10ad57739ed6a
nbain00 <zink47@gmail.com>
2022-12-01 17:20:16 -0700

    fixed slash

--- kidneyproject/settings.py
+++ kidneyproject/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://kidneycenter.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://kidneycenter.up.railway.app']
 
 
 # Application definition
